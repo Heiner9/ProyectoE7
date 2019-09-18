8d36212ae628d9581570ea9849edd4b5e1babe78
unknown <danielbricenogamboa@gmail.com>
2019-09-18 12:53:43 -0500

    Operacion Area de un Circulo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace prueba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, pi, radio;
+
+            pi = 3.1416; 
+            radio = 10.5;
+
+            area = pi * radio * radio;
+            
+
+            Console.Write("el area del circulo es:" + area);
+            
+        }
+    }
+}
